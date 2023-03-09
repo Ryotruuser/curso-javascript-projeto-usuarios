e00eed5f6106ebf00facbf84917310519d5d7e62
Ryotruuser <ryonantruuser@gmail.com>
2023-03-09 17:49:51 -0300

    validando formulario

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -21,6 +21,8 @@ class UserController {
 
             let values = this.getValues();
             
+            if(!values) return false;
+
             this.getPhoto().then(
                 (content) => {
 
@@ -83,8 +85,16 @@ class UserController {
     getValues(){
 
         let user = {};
+        let isValid = true;
 
         [...this.formEl.elements].forEach(function(field, index){
+
+            if(['name','email','password'].indexOf(field.name) > -1 && !field.values){
+
+                field.parentElement.classList.add('has-error');
+                isValid = false;
+
+            }
 
             if (field.name === "gender") {
     
@@ -103,7 +113,11 @@ class UserController {
             }
     
         });
-    
+        
+        if(!isValid){
+            return false;
+        }
+
         return new User(
             user.name, 
             user.gender, 
