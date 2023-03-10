441f894e21f760d102c99a1e2e0f5d8fce8f9cb6
Ryotruuser <ryonantruuser@gmail.com>
2023-03-10 18:13:19 -0300

    mais edições

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,11 +1,22 @@
 class UserController {
 
-    constructor(formId, tableId){
+    constructor (formId, tableId){
 
         this.formEl = document.getElementById(formId);
         this.tableEl = document.getElementById(tableId);
 
         this.onSubmit();
+        this.onEdit();
+
+    }
+
+    onEdit(){
+
+        document.querySelector("#box-user-update .btn-cancel").addEventListener("click", e => {
+
+            this.showPanelCreate();
+
+        });
 
     }
 
@@ -20,8 +31,8 @@ class UserController {
             btn.disabled = true;
 
             let values = this.getValues();
-            
-            if(!values) return false;
+
+            if (!values) return false;
 
             this.getPhoto().then(
                 (content) => {
@@ -33,54 +44,54 @@ class UserController {
                     this.formEl.reset();
 
                     btn.disabled = false;
-                },                 
+
+                }, 
                 (e) => {
-
-                    console.error(e);
-
-            })// fim getPhoto then
-
-            
+                    console.error(e)
+                }
+            );
+        
         });
 
-    }//fim onSubmit
+    }
 
     getPhoto(){
 
-        return new Promise((resolve, reject)=>{
+        return new Promise((resolve, reject) => {
 
             let fileReader = new FileReader();
 
             let elements = [...this.formEl.elements].filter(item => {
 
-            if (item.name === 'photo'){
-                return item;
-            }
+                if (item.name === 'photo') {
+                    return item;
+                }
 
             });
 
             let file = elements[0].files[0];
 
-            fileReader.onload = () =>{
+            fileReader.onload = () => {
 
                 resolve(fileReader.result);
 
             };
 
-            fileReader.onerror = () => {
-                
+            fileReader.onerror = (e) => {
+
                 reject(e);
 
-            }
+            };
 
-            if(file){
+            if(file) {
                 fileReader.readAsDataURL(file);
-            } else{
+            } else {
                 resolve('dist/img/boxed-bg.jpg');
             }
-        });//fim return promise
 
-    }//fim getPhoto
+        });
+
+    }
 
     getValues(){
 
@@ -89,10 +100,10 @@ class UserController {
 
         [...this.formEl.elements].forEach(function(field, index){
 
-            if(['name','email','password'].indexOf(field.name) > -1 && !field.values){
+            if (['name', 'email', 'password'].indexOf(field.name) > -1 && !field.value) {
 
-                field.parentElement.classList.add('has-error');
-                isValid = false;
+                field.parentElement.classList.add("has-error");
+                isValid = false
 
             }
 
@@ -102,7 +113,7 @@ class UserController {
                     user[field.name] = field.value
                 }
     
-            } else if(field.name === "admin"){
+            } else if(field.name == "admin") {
 
                 user[field.name] = field.checked;
 
@@ -113,11 +124,11 @@ class UserController {
             }
     
         });
-        
-        if(!isValid){
+
+        if (!isValid) {
             return false;
         }
-
+    
         return new User(
             user.name, 
             user.gender, 
@@ -129,28 +140,87 @@ class UserController {
             user.admin
         );
 
-    }//fim getValues
-
+    }
+    
     addLine(dataUser) {
 
         let tr = document.createElement('tr');
 
+        tr.dataset.user = JSON.stringify(dataUser);
+
         tr.innerHTML = `
-        <tr>
-            <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
-            <td>${dataUser.name}</td>
-            <td>${dataUser.email}</td>
-            <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
-            <td>${dataUser.register.toLocaleDateString('pt-br')}</td>
-            <td>
-                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-            </td>
-        </tr>
-    `;
-    this.tableEl.appendChild(tr);
-    
-    }//fim addLine
+            <tr>
+                <td><img src=${dataUser.photo} class="img-circle img-sm"></td>
+                <td>${dataUser.name}</td>
+                <td>${dataUser.email}</td>
+                <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
+                <td>${dataUser.register.toLocaleDateString('pt-br')}</td>
+                <td>
+                    <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
+                    <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                </td>
+            </tr>
+        `;
+
+        tr.querySelector(".btn-edit").addEventListener("click", e => {
+
+            let json = JSON.parse(tr.dataset.user);
+            let form = document.querySelector("#form-user-update");
+
+            for (let name in json) {
+
+                let field = form.querySelector("[name=" + name.replace("_", "") + "]");
+
+                if (field) {
+
+                    if (field.type === "file") continue;
+
+                    field.value = json[name];
+                }
 
 
-}//fim UserController
+            }
+            
+            this.showPanelUpdate();
+
+        });
+
+        this.tableEl.appendChild(tr);
+
+        this.updateCount();
+
+    }
+
+    showPanelCreate(){
+
+        document.querySelector("#box-user-create").style.display = "block";
+        document.querySelector("#box-user-update").style.display = "none";
+
+    }
+
+    showPanelUpdate(){
+
+        document.querySelector("#box-user-create").style.display = "none";
+        document.querySelector("#box-user-update").style.display = "block";
+
+    }
+
+    updateCount(){
+
+        let numberUsers = 0;
+        let numberAdmin = 0;
+
+        [...this.tableEl.children].forEach(tr => {
+
+            numberUsers++;
+
+            let user = JSON.parse(tr.dataset.user);
+
+            if (user._admin) numberAdmin++;
+        })
+
+        document.querySelector("#number-users").innerHTML = numberUsers;
+        document.querySelector("#number-users-admin").innerHTML = numberAdmin;
+
+    }
+}
